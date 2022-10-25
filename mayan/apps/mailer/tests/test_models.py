9e13e3c25bab16a915c882a2346de7ed73a8b9ed
Roberto Rosario <roberto.rosario@mayan-edms.com>
2022-10-25 17:30:15 -0400

    Fix import
    Document version export code is now in a different app.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/mailer/tests/test_models.py
+++ mayan/apps/mailer/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.core import mail
 
-from mayan.apps.documents.literals import DOCUMENT_VERSION_EXPORT_MIMETYPE
+from mayan.apps.document_exports.literals import DOCUMENT_VERSION_EXPORT_MIMETYPE
 from mayan.apps.documents.tests.base import GenericDocumentTestCase
 from mayan.apps.testing.tests.base import BaseTestCase
 
