3238f4da1f7b81b2a70f701f97e5125e8d459479
Roberto Rosario <roberto.rosario@mayan-edms.com>
2022-11-22 01:41:35 -0500

    Code style update
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/quotas/tests/test_copying.py
+++ mayan/apps/quotas/tests/test_copying.py
@@ -4,9 +4,7 @@ from mayan.apps.testing.tests.base import BaseTestCase
 from .mixins import QuotaTestMixin
 
 
-class QuotaCopyTestCase(
-    QuotaTestMixin, ObjectCopyTestMixin, BaseTestCase
-):
+class QuotaCopyTestCase(QuotaTestMixin, ObjectCopyTestMixin, BaseTestCase):
     def setUp(self):
         super().setUp()
         self._create_test_quota_with_mixins()
