f0ffab98c2507f19924cfad4482c96222816263f
Roberto Rosario <roberto.rosario@mayan-edms.com>
2023-03-11 19:20:13 -0500

    Update build string
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'Mayan EDMS'
 __version__ = '4.4.5'
 __build__ = 0x040405
-__build_string__ = 'v4.4.4-52-g38f4170e43_Sat Mar 11 17:57:43 2023 -0500'
+__build_string__ = 'v4.4.5_Sat Mar 11 19:19:16 2023 -0500'
 __django_version__ = '3.2'
 __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
